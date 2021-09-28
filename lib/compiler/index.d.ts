cc31d7732d10134a5eca2bebab6a705e1a58ee71
gityoog <907850084@qq.com>
2021-09-28 10:21:53 +0800

    构建和运行时分开打包

--- lib/compiler/index.d.ts
+++ lib/compiler/index.d.ts
@@ -2,5 +2,7 @@ import MPEntryPlugin from './entry/plugin';
 import ProcessPlugin from './utils/process-plugin';
 import fixVue3This from './utils/ts-transformer/fix-vue3-this';
 import TsconfigPathsWebpackContextPlugin from './utils/tsconfig-paths-webpack-context-plugin';
-export { ProcessPlugin, fixVue3This, TsconfigPathsWebpackContextPlugin };
+declare const valueLoader: string;
+declare const tsxLoader: string;
+export { ProcessPlugin, fixVue3This, TsconfigPathsWebpackContextPlugin, valueLoader, tsxLoader };
 export default MPEntryPlugin;
