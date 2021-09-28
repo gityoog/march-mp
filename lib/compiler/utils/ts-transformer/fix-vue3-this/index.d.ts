f826667560e7d1aee4e4be0e1d1b9c4725c70985
gityoog <907850084@qq.com>
2021-09-28 11:22:33 +0800

    移除非必要插件

--- lib/compiler/utils/ts-transformer/fix-vue3-this/index.d.ts
+++
@@ -1,3 +0,0 @@
-import ts from 'typescript';
-declare const fixVue3This: ts.TransformerFactory<ts.SourceFile>;
-export default fixVue3This;
--- src/compiler/utils/value-loader/index.ts
+++
@@ -1,3 +0,0 @@
-export default function (source: string) {
-  return `${JSON.stringify(source)}`
-}
